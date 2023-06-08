737e27449ed0f7d4fd75c62c153015d555a1dc17
Myeung Jin Lee <66550892+mjlee111@users.noreply.github.com>
2023-06-09 04:43:34 +0900

    Add files via upload

---
+++ opencv_installer.sh
@@ -0,0 +1,156 @@
+function custom_echo() {
+  text=$1
+  color=$2
+
+  case $color in
+    "green")
+      echo -e "\033[32m[RO:BIT] $text\033[0m"
+      ;;
+    "red")
+      echo -e "\033[31m [RO:BIT] $text\033[0m"
+      ;;
+    *)
+      echo "$text"
+      ;;
+  esac
+}
+
+loading_animation() {
+  local interval=1
+  local duration=30
+  local bar_length=$(tput cols)  # 터미널 창의 너비를 가져옴
+  local total_frames=$((duration * interval))
+  local frame_chars=("█" "▉" "▊" "▋" "▌" "▍" "▎" "▏")
+
+  for ((i = 0; i <= total_frames; i++)); do
+    local frame_index=$((i % ${#frame_chars[@]}))
+    local progress=$((i * bar_length / total_frames))
+    local bar=""
+    for ((j = 0; j < bar_length; j++)); do
+      if ((j <= progress)); then
+        bar+="${frame_chars[frame_index]}"
+      else
+        bar+=" "
+      fi
+    done
+    printf "\r\033[32m%s\033[0m" "$bar"  # 초록색으로 출력
+    sleep 0.$interval
+  done
+
+  printf "\n"
+}
+
+
+
+
+custom_echo "RO:BIT 17th Myeungjin Lee" "green"
+custom_echo "OpenCV Auto Installer" "green"
+
+loading_animation
+
+cd 
+
+custom_echo "Checking if OpneCV is already installed" "green"
+if pkg-config --exists opencv; then
+  opencv_version=$(pkg-config --modversion opencv)
+  custom_echo "OpenCV already installed!!! current OpenCV version : "$opencv_version"" "red"
+  custom_echo "Delete current version? If not, installation ends" "red"
+  read -p "Press Enter to delete the current version and proceed, or any other key to exit: " choice
+
+  if [ "$choice" == "" ]; then
+    echo "Deleting the current version of OpenCV..."
+    sudo apt-get purgeopencv*
+    sudo apt-get purgelibopencv*
+    sudo apt-get autoremove
+    sudo find /usr/local/ -name "*opencv*" -exec rm -i {} \;
+    echo "Current version of OpenCV has been deleted."
+  else
+    echo "Installation stopped. Current version of OpenCV was not deleted."
+    exit
+  fi
+else
+  echo "OpenCV is not installed"
+fi
+
+custom_echo "Installing requirements" "green"
+loading_animation
+sudo apt update -y
+sudo apt upgrade -y
+sudo apt-get install -y build-essential cmake
+sudo apt-get install -y pkg-config
+sudo apt-get install -y libjpeg-dev libtiff5-dev libpng-dev
+sudo apt-get install -y ffmpeg libavcodec-dev libavformat-dev libswscale-dev libxvidcore-dev libx264-dev libxine2-dev
+sudo apt-get -y install libv4l-dev v4l-utils
+sudo apt-get -y install libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev
+sudo apt-get -y install libqt4-dev
+sudo apt-get -y install mesa-utils libgl1-mesa-dri libgtkgl2.0-dev libgtkglext1-dev
+sudo apt-get -y install libatlas-base-dev gfortran libeigen3-dev
+
+
+mkdir opencv && cd opencv
+
+loading_animation
+custom_echo "Enter your target OpenCV version" "green"
+read cvversion
+
+custom_echo "${cvversion}" "red"
+custom_echo "checking if version is available ..." "green"
+loading_animation
+download_url="https://github.com/opencv/opencv/archive/${cvversion}.zip"
+wget -O opencv.zip "$download_url" || { custom_echo "This version is unavailable" "red" ; cd && rm -rf opencv; exit 1; }
+download_url_contrib="https://github.com/opencv/opencv_contrib/archive/${cvversion}.zip"
+wget -O opencv_contrib.zip "$download_url_contrib" || { custom_echo "This version is unavailable" "red" ; cd && sudo rm -rf opencv; exit 1; }
+
+unzip opencv.zip
+unzip opencv_contrib.zip
+
+dest_dirs="opencv-${cvversion}"
+cd "$dest_dirs"
+mkdir build && cd build
+
+custom_echo "VERSION AVAILABLE" "green"
+custom_echo "Downloaded required files" "green"
+custom_echo "now building ... "
+
+cmake_command="cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local -D WITH_TBB=OFF -D WITH_IPP=OFF -D WITH_1394=OFF -D BUILD_WITH_DEBUG_INFO=OFF -D BUILD_DOCS=OFF -D INSTALL_C_EXAMPLES=ON -D INSTALL_PYTHON_EXAMPLES=ON -D BUILD_EXAMPLES=OFF -D BUILD_PACKAGE=OFF -D BUILD_TESTS=OFF -D BUILD_PERF_TESTS=OFF -D WITH_QT=OFF -D WITH_GTK=ON -D WITH_OPENGL=ON -D BUILD_opencv_python3=ON -D OPENCV_EXTRA_MODULES_PATH=../../opencv_contrib-4.5.2/modules -D WITH_V4L=ON -D WITH_FFMPEG=ON -D WITH_XINE=ON -D OPENCV_ENABLE_NONFREE=ON -D BUILD_NEW_PYTHON_SUPPORT=ON -D OPENCV_SKIP_PYTHON_LOADER=ON -D OPENCV_GENERATE_PKGCONFIG=ON ../"
+
+custom_echo "Running cmake command..." "green"
+$cmake_command || { custom_echo "BUILD FAILED !!!! MANUAL INSTALL REQUIRED !!!!" "red"; exit 1; }
+
+custom_echo "Running make command..." "green"
+time make -j $(nproc) || { custom_echo "BUILD FAILED !!!! MANUAL INSTALL REQUIRED !!!!" "red"; exit 1; }
+
+sudo make install
+
+if ! grep -q '/usr/local/lib' /etc/ld.so.conf.d/*; then
+  sudo sh -c 'echo "/usr/local/lib" > /etc/ld.so.conf.d/opencv.conf'
+fi
+sudo ldconfig
+custom_echo "BUILD COMPLETE" "green"
+loading_animation
+cd && sudo rm -rf opencv
+
+echo " "
+echo " "
+echo " "
+custom_echo "There might be some removed packaged for ROS. Install them?" "green"
+read -p "Press Enter to install and proceed, or any other key to exit: " choice
+if ["$choice" == "" ]; then
+  ros_version=$(rosversion -d)
+  custom_echo "${ros_version}" "red"
+  sudo apt-get install -y ros-$ros_version-cv-bridge
+  sudo apt-get install -y ros-$ros_version-image-view
+  sudo apt-get install -y rosbash
+  sudo apt-get install -y ros-$ros_version-usb-cam
+  custom_echo "ROS packages installed completely" "green"
+  custom_ehco "PLEASE EDIT CV_BRIDGE CMAKE FILE" "red"
+fi 
+
+
+loading_animation
+cd
+sudo rm -rf OpenCV_auto_installer
+echo "COMPLETE"
+
+
+
